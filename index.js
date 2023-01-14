3e0d8c0ec3165b68532f4dee5c5ba022fcb02929
M-D1 <115486327+M-D1@users.noreply.github.com>
2023-01-14 12:52:31 +0300

    .

---
+++ index.js
@@ -0,0 +1,96 @@
+import theme from './theme'
+theme()
+
+console.log(window.innerWidth)
+
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+const numbers =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const symbols =  ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
+
+const checkboxNum = document.getElementById('numbers')
+const checkboxSym = document.getElementById('symbols')
+
+const btn = document.getElementById('btn')
+const input = document.querySelector('.input-number')
+
+const passOne = document.getElementById('passOne')
+const passTwo = document.getElementById('passTwo')
+
+
+// creating a function that loops over the symbols/numbers arrays
+// and add one of these arrays or
+// both of them to the original array
+
+function loop(array){
+    for(let i = 0; i < array.length; i++){
+        characters.push(array[i])
+    }  
+}
+// creating a function that removes the number/symbols arrays 
+//when the use uncheck the checkboc
+function indexNumbers(index){
+    const indexNum = characters.indexOf(index)
+    
+    if(indexNum > -1) characters.splice(indexNum)   
+}
+
+
+checkboxSym.addEventListener('click',function(){
+
+    if(checkboxSym.checked) loop(symbols)
+        
+    else{
+         indexNumbers("~")
+    }
+    
+})
+
+
+
+checkboxNum.addEventListener('click',function(){
+
+    if(checkboxNum.checked) loop(numbers)
+    
+    else{
+        indexNumbers('0')
+    } 
+ })
+
+
+
+
+
+function generatePassWord(value){
+    if(input.value <= 0){
+        alert('please enter the length of a password')
+        
+    }else{
+        let passWord = ''
+        let passWord2 = ''
+        
+        //looping by the number the user want 
+        //each iteration a random charecter is added to the password variables
+        for(let i = 0; i < value; i++){
+            passWord +=
+                characters[Math.floor(Math.random()*characters.length)]
+    
+            passWord2 +=
+                characters[Math.floor(Math.random()*characters.length)]
+        }
+            
+            // displaying the password
+            passOne.textContent = passWord
+            passTwo.textContent = passWord2
+    }
+   
+ 
+}
+
+
+btn.addEventListener('click',function(){
+    generatePassWord(input.value)
+})
+
+
---
+++ theme.js
@@ -0,0 +1,44 @@
+// javascript
+function themes(){
+const spanContainr = document.querySelector('.span-container');
+const span = document.querySelector('#span')
+const body = document.body
+console.log(body)
+// console.log(span)
+
+ let theme = 'light'
+
+ 
+
+spanContainr.addEventListener('click',function(){
+     
+    
+    if(theme == 'light'){
+        theme = 'dark'
+    }else{
+        theme = 'light'
+    }
+  
+    
+     const white = document.querySelectorAll('.color')
+      
+     white.forEach(color => {
+     
+         if(theme == 'light'){
+            color.classList.remove('white')
+            span.classList.remove('light')
+            span.classList.add('dark')
+         }else{
+            color.classList.add('white')
+            span.classList.remove('dark')
+            span.classList.add('light')
+         }
+     })
+       
+       body.style.backgroundColor = theme == 'light'? '#ECFDF5' : '#222'
+       
+      
+})
+}
+
+export default themes 
